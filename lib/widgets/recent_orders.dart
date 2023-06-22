38cea3116c424b3af0349fcee9a62dd5c313881f
Abdul <awajidm@outlook.com>
2023-06-22 14:36:48 +0500

    first commit after creating home screen and recent orders widget

---
+++ lib/widgets/recent_orders.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/models/order.dart';
+
+class RecentOrders extends StatelessWidget {
+
+  _buildRecentOrder(BuildContext contect, Order order){
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: 320.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(width: 1.0,color: Colors.grey[500])
+      ),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            'Recent Orders',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        Container(
+          height: 120.0,
+          color: Colors.amber,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders.length,
+            itemBuilder: (BuildContext context, int index) {
+              Order order = currentUser.orders[index];
+              return _buildRecentOrder(context, order);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
